e30d35edff8536e0377f615dc40654f289d4fb8e
Chad Oakenfold <chad@oakenfold.ca>
2022-05-08 21:11:55 -0400

    update remix config

--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,8 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "api/_build",
 };
