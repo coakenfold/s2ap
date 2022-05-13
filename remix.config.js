59c554fcc5460e4d41eb20adfbdf3ab474846818
Chad Oakenfold <chad@oakenfold.ca>
2022-05-13 12:17:35 -0400

    namespacing build directory

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   appDirectory: "app",
-  browserBuildDirectory: "public/s2ap",
-  publicPath: "/s2ap/",
+  browserBuildDirectory: "public/build.s2ap",
+  publicPath: "/build.s2ap/",
   serverBuildDirectory: "api/_build",
 };
