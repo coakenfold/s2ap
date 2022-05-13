767da1458c319137c3a5cb788c6e9059d3cb999b
Chad Oakenfold <chad@oakenfold.ca>
2022-05-13 12:09:40 -0400

    changing public/build folder to see if I can rewrite against it

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
+  browserBuildDirectory: "public/s2ap",
+  publicPath: "/s2ap/",
   serverBuildDirectory: "api/_build",
 };
