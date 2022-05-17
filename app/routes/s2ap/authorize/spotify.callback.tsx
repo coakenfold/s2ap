29a72f68519f3d01571c8b6a53d1886b242870c8
Chad Oakenfold <chad@oakenfold.ca>
2022-05-16 22:58:36 -0400

    spotify auth try 2

--- app/routes/s2ap/authorize/spotify.callback.tsx
+++ app/routes/s2ap/authorize/spotify.callback.tsx
@@ -1,14 +1,15 @@
-// import type { LoaderFunction } from "@remix-run/node";
-// import { authenticator } from "~/models/auth.server";
+import type { LoaderFunction } from "@remix-run/node";
+import { authenticator } from "~/models/auth.server";
 
-// export const loader: LoaderFunction = ({ request }) => {
-//   console.log("@@ authorize/spotify/callback");
-//   const output = authenticator.authenticate("spotify", request, {
-//     successRedirect: "/s2ap?status=success",
-//   });
-//   console.log("@@ authorize/spotify/callback:output", output);
-//   return output;
-// };
+export const loader: LoaderFunction = ({ request }) => {
+  const output = authenticator.authenticate("spotify", request, {
+    successRedirect: "/s2ap?status=success",
+  });
+  console.log("authorize/spotify/callback >>> output =", output);
+  console.log("authorize/spotify/callback >>> request =", request);
+  return output;
+};
+
 export default function Spotify() {
   return (
     <div className="flex justify-center p-8">
--- app/routes/s2ap/authorize/spotify.tsx
+++ app/routes/s2ap/authorize/spotify.tsx
@@ -2,10 +2,5 @@ import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { authenticator } from "~/models/auth.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  console.log("authorize/spotify:output");
-  const output = await authenticator.authenticate("spotify", request, {
-    successRedirect: "/s2ap?status=success",
-  });
-  console.log("@@ authorize/spotify:output", output);
-  return output;
+  return await authenticator.authenticate("spotify", request);
 };
