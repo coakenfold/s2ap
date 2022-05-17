3f153339762d62242315553c47647c843fa6f08f
Chad Oakenfold <chad@oakenfold.ca>
2022-05-16 22:43:40 -0400

    good old logging

--- app/routes/s2ap/authorize/spotify.callback.tsx
+++ app/routes/s2ap/authorize/spotify.callback.tsx
@@ -2,16 +2,10 @@ import type { LoaderFunction } from "@remix-run/node";
 import { authenticator } from "~/models/auth.server";
 
 export const loader: LoaderFunction = ({ request }) => {
-  return authenticator.authenticate("spotify", request, {
+  console.log("@@ authorize/spotify/callback");
+  const output = authenticator.authenticate("spotify", request, {
     successRedirect: "/s2ap?status=success",
-    // failureRedirect: "/s2ap?status=error",
   });
+  console.log("@@ authorize/spotify/callback:output", output);
+  return output;
 };
-
-export default function SpotifyCallback() {
-  return (
-    <div className="flex justify-center p-8">
-      s2ap/authorize/spotify/callback
-    </div>
-  );
-}
--- app/routes/s2ap/authorize/spotify.tsx
+++ app/routes/s2ap/authorize/spotify.tsx
@@ -2,17 +2,10 @@ import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { authenticator } from "~/models/auth.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  const out = await authenticator.authenticate("spotify", request, {
+  console.log("authorize/spotify:output");
+  const output = await authenticator.authenticate("spotify", request, {
     successRedirect: "/s2ap?status=success",
   });
-  return out;
+  console.log("@@ authorize/spotify:output", output);
+  return output;
 };
-
-export function ErrorBoundary({ error }) {
-  console.error(error);
-  return <div>Error</div>;
-}
-
-export default function Spotify() {
-  return <div className="flex justify-center p-8">s2ap/authorize/spotify</div>;
-}
