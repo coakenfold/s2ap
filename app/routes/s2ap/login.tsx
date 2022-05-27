a418a7a88496a1dbce36f1cb56cff7f16a7497dd
Chad Oakenfold <chad@oakenfold.ca>
2022-05-27 09:38:40 -0400

    /s2ap/login

---
+++ app/routes/s2ap/login.tsx
@@ -0,0 +1,69 @@
+import { /*redirect,*/ json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { userPrefs } from "~/cookies";
+
+import type { LoaderFunction } from "@remix-run/node";
+
+// import type { ActionFunction } from "@remix-run/node";
+// export const action: ActionFunction = async ({ request }) => {
+//   const cookieHeader = request.headers.get("Cookie");
+//   const cookie = (await userPrefs.parse(cookieHeader)) || {};
+
+//   const form = await request.formData();
+//   const formCanRequestEmail = !!(form.get("canRequestEmail") as
+//     | string
+//     | undefined);
+
+//   cookie.canRequestEmail = formCanRequestEmail;
+
+//   return redirect(`/s2ap/authorize/spotify`, {
+//     headers: {
+//       "Set-Cookie": await userPrefs.serialize(cookie),
+//     },
+//   });
+// };
+
+export interface LoaderOutput {
+  build: string | undefined;
+  canRequestEmail: boolean;
+}
+export const loader: LoaderFunction = async ({ request }) => {
+  const build = process.env.BUILD;
+  const cookieHeader = request.headers.get("Cookie");
+  const cookie = (await userPrefs.parse(cookieHeader)) || {};
+  return json({ build, canRequestEmail: cookie.canRequestEmail });
+};
+export default function Index() {
+  const { build, canRequestEmail } = useLoaderData<LoaderOutput>();
+  return (
+    <div className="flex justify-center p-8">
+      <main className="w-full max-w-3xl">
+        <h1
+          className="font-light text-slate-900 dark:text-slate-50"
+          title="Song to Album Playlist"
+        >
+          S2ap
+        </h1>
+        <Form method="post" action="/s2ap/authorize/spotify">
+          <input
+            type="checkbox"
+            id="canRequestEmail"
+            name="canRequestEmail"
+            defaultChecked={canRequestEmail}
+          />
+          <label htmlFor="canRequestEmail">Share my email with S2Ap</label>
+          <button className="dark:highlight-white/20 my-2 flex h-14 w-full items-center justify-center rounded-lg bg-slate-900 px-6 text-xl font-semibold text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 dark:bg-sky-500 dark:hover:bg-sky-400 sm:w-auto">
+            Log in with Spotify
+          </button>
+        </Form>
+        <div>
+          {build ? (
+            <small className="mt-16 mb-8 block bg-slate-900 text-right text-xs text-slate-800 ">
+              {build}
+            </small>
+          ) : null}
+        </div>
+      </main>
+    </div>
+  );
+}
