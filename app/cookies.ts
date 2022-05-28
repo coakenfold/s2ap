cdde2c8fa8fb7171ac55668831b02d2efa2842fe
Chad Oakenfold <chad@oakenfold.ca>
2022-05-27 22:38:34 -0400

    add interface and default state

--- app/cookies.ts
+++ app/cookies.ts
@@ -4,8 +4,14 @@ export const userPrefs = createCookie("user-prefs", {
   maxAge: 604_800, // one week
 });
 
+export interface UserPrefs {
+  canRequestEmail: boolean;
+}
+export const userPrefsDefault = {
+  canRequestEmail: true,
+};
 export const getUserPrefs = async (request: Request) => {
   const cookieHeader = request.headers.get("Cookie");
-  const cookie = (await userPrefs.parse(cookieHeader)) || {};
-  return cookie;
+  const cookie = (await userPrefs.parse(cookieHeader)) || userPrefsDefault;
+  return cookie as UserPrefs;
 };
