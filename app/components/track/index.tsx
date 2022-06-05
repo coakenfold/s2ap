fbfa9881de732fdf343011fc83d0b1d35f767079
Chad Oakenfold <chad@oakenfold.ca>
2022-06-05 18:02:52 -0400

    Attribution links

--- app/components/track/index.tsx
+++ app/components/track/index.tsx
@@ -6,7 +6,78 @@ import type {
   SimplifiedAlbum,
 } from "spotify-types";
 import { MiniForm } from "~/components/MiniForm";
-// import { trackMachine } from "./track.machine";
+
+const Attribution = ({ item }: { item: SpotifyTrack }) => {
+  const trackTitle = item?.name;
+  const trackUrl = item?.external_urls?.spotify;
+  const styleLink = "dark:text-sky-40 mr-2 text-sky-500 hover:text-sky-200";
+  const trackArtists = item
+    ? item?.artists.map(({ name, external_urls, id }) => (
+        <a key={id} href={external_urls.spotify} className={styleLink}>
+          {name}
+        </a>
+      ))
+    : undefined;
+
+  const album = item?.album;
+  const albumTitle = album.name;
+  const albumUrl = album?.external_urls.spotify;
+  const heading = "font-bold inline-block mx-2";
+  return (
+    <section className="flex">
+      <a href="https://spotify.com/" className="my-2 mr-2 inline-block">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="68px"
+          width="68px"
+          version="1.1"
+          viewBox="0 0 168 168"
+        >
+          <path
+            fill="#1ED760"
+            d="m83.996 0.277c-46.249 0-83.743 37.493-83.743 83.742 0 46.251 37.494 83.741 83.743 83.741 46.254 0 83.744-37.49 83.744-83.741 0-46.246-37.49-83.738-83.745-83.738l0.001-0.004zm38.404 120.78c-1.5 2.46-4.72 3.24-7.18 1.73-19.662-12.01-44.414-14.73-73.564-8.07-2.809 0.64-5.609-1.12-6.249-3.93-0.643-2.81 1.11-5.61 3.926-6.25 31.9-7.291 59.263-4.15 81.337 9.34 2.46 1.51 3.24 4.72 1.73 7.18zm10.25-22.805c-1.89 3.075-5.91 4.045-8.98 2.155-22.51-13.839-56.823-17.846-83.448-9.764-3.453 1.043-7.1-0.903-8.148-4.35-1.04-3.453 0.907-7.093 4.354-8.143 30.413-9.228 68.222-4.758 94.072 11.127 3.07 1.89 4.04 5.91 2.15 8.976v-0.001zm0.88-23.744c-26.99-16.031-71.52-17.505-97.289-9.684-4.138 1.255-8.514-1.081-9.768-5.219-1.254-4.14 1.08-8.513 5.221-9.771 29.581-8.98 78.756-7.245 109.83 11.202 3.73 2.209 4.95 7.016 2.74 10.733-2.2 3.722-7.02 4.949-10.73 2.739z"
+          />
+        </svg>
+      </a>
+      <table>
+        <tr>
+          <td className={heading}>Song</td>
+          <td>
+            <a className={styleLink} href={trackUrl}>
+              {trackTitle}
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td className={heading}>Album</td>
+          <td>
+            <a className={styleLink} href={albumUrl}>
+              {albumTitle}
+            </a>
+          </td>
+        </tr>
+
+        {item?.artists.length < 2 ? (
+          <tr>
+            <td className={heading}>Artist</td>
+            <td>{trackArtists}</td>
+          </tr>
+        ) : (
+          <tr>
+            <td className={heading}>Artists</td>
+            <td>
+              <ul>
+                {trackArtists?.map((artistLink, index) => {
+                  return <li key={`artist__${index}`}>{artistLink}</li>;
+                })}
+              </ul>
+            </td>
+          </tr>
+        )}
+      </table>
+    </section>
+  );
+};
 const determineIfAlbum = (album: SimplifiedAlbum) => {
   // Definitely an album
   if (album?.album_type === "album" && album?.total_tracks > 2) {
@@ -35,20 +106,10 @@ export interface TrackInput {
   playlistId?: string;
 }
 export function Track({ isSubmitting, current, user, playlistId }: TrackInput) {
-  // const [current, send] = useMachine(trackMachine);
-  // console.log("machine", { current });
   const item = current?.item as SpotifyTrack;
   const trackTitle = item?.name;
   const trackArtists = item
-    ? item?.artists.map(({ name, external_urls, id }) => (
-        <a
-          key={id}
-          href={external_urls.spotify}
-          className="dark:text-sky-40 mr-2 text-sky-500 hover:text-sky-200"
-        >
-          {name}
-        </a>
-      ))
+    ? item?.artists.map(({ name, external_urls, id }) => name).join(", ")
     : undefined;
   const album = item?.album;
 
@@ -56,7 +117,6 @@ export function Track({ isSubmitting, current, user, playlistId }: TrackInput) {
   const albumTitle = album?.name;
   const albumId = album?.id;
   const albumTracks = album?.total_tracks;
-  const albumUrl = album?.external_urls.spotify;
 
   return (
     <div className="py-4">
@@ -126,7 +186,7 @@ export function Track({ isSubmitting, current, user, playlistId }: TrackInput) {
           <h2 className="font-semibold text-indigo-500 dark:text-indigo-400">
             Currently listening to
           </h2>
-          <ul>
+          <ul className="mb-24">
             <li className="text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-50 sm:text-4xl">
               {trackTitle}
             </li>
@@ -144,16 +204,8 @@ export function Track({ isSubmitting, current, user, playlistId }: TrackInput) {
                 />
               </li>
             ) : null}
-
-            <li className="max-w-3xl space-y-6">
-              <a
-                href={albumUrl}
-                className="dark:text-sky-40 text-lg text-sky-500 hover:text-sky-200 sm:text-lg"
-              >
-                {albumTitle}
-              </a>
-            </li>
           </ul>
+          <Attribution item={item} />
         </>
       ) : (
         <div className="py-4">Not listening to anything at the moment</div>
